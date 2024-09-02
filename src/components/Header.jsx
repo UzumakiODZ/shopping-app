ec545e310680fad1d047666fb3236c659bd30054
UzumakiODZ <ps2357270@gmail.com>
2024-09-02 22:21:32 +0530

    empty

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "/src/styles/Header.css";
 import { Link } from 'react-router-dom';
+import CartImage from '/src/assets/shopping-cart-outline.svg';
 
 const Header = ({ cartItemCount }) => {
   return (
@@ -10,7 +11,7 @@ const Header = ({ cartItemCount }) => {
         <div className='nav-pages'>
           <Link className='nav-links' to="/">Home</Link>
           <Link className='nav-links' to="/cart">
-            <img src="/src/assets/shopping-cart-outline.svg" alt="Cart" height="50px"/>
+            <img src={CartImage} alt="Cart" height="50px"/>
              <span className="cart-count">{cartItemCount}</span>
           </Link>
         </div>
